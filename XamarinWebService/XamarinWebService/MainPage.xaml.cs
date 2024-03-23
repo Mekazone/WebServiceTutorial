43b173c353ac8a95fabf0c23f97c8cce4c84c157
Emeka Ozonze <mekaz.ozonze@gmail.com>
2024-03-23 00:31:14 +0000

    Initial commit

---
+++ XamarinWebService/XamarinWebService/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using MySql.Data.MySqlClient;
+
+namespace XamarinWebService
+{
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+        }
+
+        //private async void Button_Clicked(object sender, EventArgs e)
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //List<Repository> repositories = await _restService.GetRepositoriesAsync(Constants.GitHubReposEndpoint);
+            //collectionView.ItemsSource = repositories;
+            string cs = @"server=localhost;userid=root;password=;database=stockkip_db";
+            var con = new MySqlConnection(cs);
+            con.Open();
+            Console.WriteLine($"MySQL version : {con.ServerVersion}");
+            label.Text = "test";
+        }
+    }
+}
